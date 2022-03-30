401164c3d88bd7208881c2cbab1ea5d68f90af09
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-03-29 21:44:08 -0700

    Still can repro!

--- jfly-package/setup.py
+++ jfly-package/setup.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 
 try:
-    with open("/tmp/jfly.log", "a") as f:
+    with open("/tmp/jfly-setup.log", "a") as f:
         import sys
         import traceback
         traceback.print_stack(file=f)
-        print(f"BEFORE IMPORTING SETUPTOOLS {sys.path}", file=f)#<<<
+        print(f"Start of jfly-package/setup.py", file=f)#<<<
 
     import setuptools
-
-    with open("/tmp/jfly.log", "a") as f:
-        print(f"successfully imported SETUPTOOLS: {setuptools.__version__} {setuptools.__path__}", file=f)#<<<
 
     setuptools.setup(
         name='jfly',
@@ -20,9 +17,10 @@ try:
             'jfly',
         ],
       )
+
 except Exception as e:
     import traceback
-    with open("/tmp/jfly.log", "a") as f:
+    with open("/tmp/jfly-setup.log", "a") as f:
         print(f"catch-all found {e}", file=f)#<<<
         traceback.print_exc(file=f)
     raise e
--- repro.sh
+++ repro.sh
@@ -3,17 +3,19 @@
 set -euo pipefail
 
 reset() {
+    echo "Restting..."
+
     # Uninstall jfly and go back to an old version of setuptools (old enough
     # that pipenv will decide to upgrade it because there's a newer version in
     # Pipfile.lock)
-    pip uninstall -y jfly setuptools
+    pip uninstall -y jfly setuptools &>/dev/null
 
-    # This is more analogous to what a fresh container looks like.
+    # This would be more analogous to what a fresh container looks like.
     # (list of packages from `apt-cache depends python3-setuptools`)
     # apt-get install --reinstall python3-setuptools python3-pkg-resources python3-distutils
 
-    # This is more likely to make sense to Python folks who don't want to think about apt.
-    pip install setuptools==45.2.0
+    # But, this is more likely to make sense to Python folks who don't want to think about apt.
+    pip install setuptools==45.2.0 &>/dev/null
 }
 
 reset
@@ -22,7 +24,11 @@ reset
 #<<< pip install -U wheel
 #<<<
 
+rm -f /tmp/jfly-setup.log
 pipenv install --system --deploy --verbose
+echo "------------ START OF /tmp/jfly-setup.log ------------------------"
+cat /tmp/jfly-setup.log
+echo "------------ END OF /tmp/jfly-setup.log ------------------------"
 
 python3 -c "import jfly.BadMath as j; print(j)"
 echo "success!"
